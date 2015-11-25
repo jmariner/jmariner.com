eb43fd3728f356b20160cbb00da2b7aed29e2fb3
Justin <justin.mariner@hotmail.com>
2015-11-25 16:57:36 -0500

    created template style for home page + scss now compiles into the html in home.html

--- assets/scripts/style.js
+++ assets/scripts/style.js
@@ -1,3 +1,22 @@
 $(function(){
+	var header = document.querySelector("header");
+	var headerHeight = header.getBoundingClientRect().height;
+	var fixedHeader = $(header.cloneNode(true)).addClass("fixed").get(0);
+	document.body.insertBefore(fixedHeader, header);
 
+	var fixedVisible = false;
+
+	$(document).scroll(function() {
+		var top = this.body.scrollTop;
+		var bottom = top + window.innerHeight;
+
+		if (fixedVisible && top < headerHeight) {
+			fixedHeader.classList.remove("active");
+			fixedVisible = false;
+		}
+		if (!fixedVisible && top >= headerHeight) {
+			fixedHeader.classList.add("active");
+			fixedVisible = true;
+		}
+	});
 });
